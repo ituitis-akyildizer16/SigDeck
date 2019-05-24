79dc9dfb33812bf66de230df69921b1714a26690
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2019-05-24 18:28:00 +0200

    perf: improve armor

--- sigdeck/keys.py
+++ sigdeck/keys.py
@@ -5,3 +5,8 @@ import os
 from pathlib import Path
 
 from . import ed25519
+
+ARMOR_PUB = "-----BEGIN SIGDECK PUBLIC KEY-----"
+ARMOR_SEC = "-----BEGIN SIGDECK SECRET KEY-----"
+ARMOR_END = "-----END SIGDECK {kind} KEY-----"
+
