42ae5f27ab528a1ce62fe2e43bd4a5d6f4d6df8a
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2019-07-26 18:46:00 +0200

    fix: cryptography

--- sigdeck/verify.py
+++ sigdeck/verify.py
@@ -14,3 +14,5 @@ def verify_file(signature_path, file_path, public):
     sig = signature_path.read_bytes()
     if sig[:1] == b"-":
         from .armor import parse_signature
+        sig = parse_signature(sig.decode("utf-8"))
+    return verify_bytes(sig, file_path.read_bytes(), public)
