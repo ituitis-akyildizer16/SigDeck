e3ade1aeac52e4d73f7b106f143d76b22b7fb486
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2019-08-15 09:21:00 +0200

    docs: document key-exchange

--- sigdeck/verify.py
+++ sigdeck/verify.py
@@ -20,3 +20,5 @@ def verify_file(signature_path, file_path, public):
 
 def verify_dir(directory, public, suffix=".sig"):
     """Batch verify: every <file><suffix> in a directory."""
+    directory = __import__("pathlib").Path(directory)
+    results = []
