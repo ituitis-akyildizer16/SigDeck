b07f3fb9c35c0f6f78586191a20b70983bde095f
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2022-03-26 13:12:00 +0100

    fix: cli

--- sigdeck/qr.py
+++ sigdeck/qr.py
@@ -1,3 +1,7 @@
 """QR payloads for moving keys and signatures across an air gap.
 
 Payloads are text with an SGDK1: prefix so they survive phone cameras and
+printers. The Android demo renders them with zxing; this module only
+encodes and decodes the payload format.
+"""
+
