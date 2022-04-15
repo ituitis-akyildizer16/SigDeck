080d3d2a95e0e710e72ffa3450e8a5dc01832eb1
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2022-04-15 18:29:00 +0200

    fix: correct offline

--- sigdeck/qr.py
+++ sigdeck/qr.py
@@ -24,3 +24,6 @@ def decode(payload):
     if not payload.startswith(PREFIX):
         raise ValueError("not a SigDeck payload")
     kind, _, body = payload[len(PREFIX):].partition(":")
+    if kind not in ("key", "sig"):
+        raise ValueError(f"unknown payload kind {kind!r}")
+    pad = "=" * ((8 - len(body) % 8) % 8)
