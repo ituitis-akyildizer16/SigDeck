2c71ccb3047870534bc801326f4f076bdc9ae7dd
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2024-02-27 10:51:00 +0100

    docs: document cli

--- app/src/main/java/com/akyildizer16/sigdeck/VerifyActivity.kt
+++ app/src/main/java/com/akyildizer16/sigdeck/VerifyActivity.kt
@@ -6,3 +6,5 @@ import androidx.appcompat.app.AppCompatActivity
 
 class VerifyActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
