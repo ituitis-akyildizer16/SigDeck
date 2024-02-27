bebffe2c758cdb24b7fa65d6b1d9fae599483739
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2024-02-27 10:56:00 +0100

    feat: key-exchange

--- app/src/main/java/com/akyildizer16/sigdeck/VerifyActivity.kt
+++ app/src/main/java/com/akyildizer16/sigdeck/VerifyActivity.kt
@@ -8,3 +8,5 @@ class VerifyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify)
+
