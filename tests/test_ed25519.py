5062ea8b0df100295c5b5ed07aa535a7a3cffc29
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2025-05-03 09:27:00 +0200

    perf: improve armor

--- tests/test_ed25519.py
+++ tests/test_ed25519.py
@@ -19,3 +19,9 @@ def test_rfc8032_vector_2():
     pk = bytes.fromhex("3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c")
     msg = bytes([0x72])
     sig = bytes.fromhex(
+        "92a009a9f0d4cab8720e820b5f642540a2b27b5416503f8fb3762223ebdb69da"
+        "085ac1e43e15996e458f3613d0f11d8c387b2eaeb4302aeeb00d291612bb0c00")
+    assert public_key(seed) == pk
+    assert sign(msg, seed) == sig
+    assert verify(sig, msg, pk)
+
