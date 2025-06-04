bce23a835ffee80010b6f1f9eb12eaf1f587f133
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2025-06-04 16:00:00 +0200

    chore: qr

--- tests/test_sign_verify.py
+++ tests/test_sign_verify.py
@@ -3,3 +3,7 @@
 from sigdeck.keys import generate_seed, public_bytes
 from sigdeck.sign import sign_file, detached_path
 from sigdeck.verify import verify_file
+
+
+def test_roundtrip(tmp_path):
+    seed = generate_seed()
