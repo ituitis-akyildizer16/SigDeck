141621278d84afab4e2b08951559afc47eb65ff8
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2026-04-10 15:27:00 +0200

    chore: scrypt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,3 +28,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("com.google.zxing:core:3.5.3")
+}
